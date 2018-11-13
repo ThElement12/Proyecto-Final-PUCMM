9df73682cac68a2358b4579e771fa548b0dfcb3e
Joseph <Joseph@DESKTOP-IMSH0QM>
2018-11-13 15:36:21 -0400

    Controladora parte 2

--- Proyecto Final Programacion I/src/Logico/PUCMM.java
+++ Proyecto Final Programacion I/src/Logico/PUCMM.java
@@ -1,10 +1,14 @@
 package Logico;
 
+import java.util.ArrayList;
+
 public class PUCMM {
 	private ArrayList<Recurso> misRecursos;
 	private ArrayList<Evento> misEventos;
 
 	public PUCMM() {
+		misRecursos = new ArrayList<>();
+		misEventos = new ArrayList<>();
 	
 		
 		
