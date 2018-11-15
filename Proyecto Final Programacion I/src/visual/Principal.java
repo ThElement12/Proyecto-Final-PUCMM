9b29eb45e96d92bfd3fc5adb8b5d932cd1fef031
ruben <ruben@DESKTOP-CV5HDJ7>
2018-11-15 15:38:38 -0400

    Creacion del paquete visual -Solo la ventana Principal-
    						Att. El jefe

---
+++ Proyecto Final Programacion I/src/visual/Principal.java
@@ -0,0 +1,47 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Principal extends JFrame {
+	private Dimension dim;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal frame = new Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Principal() {
+		setResizable(false);
+		setTitle("Manejador de Eventos");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		dim = super.getToolkit().getScreenSize();
+		super.setSize(dim.width, dim.height - 50);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
