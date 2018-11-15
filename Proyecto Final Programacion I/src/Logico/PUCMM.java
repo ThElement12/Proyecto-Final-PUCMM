be12a491d8fb81ba728539c8a7b4b3164972e82a
Joseph <Joseph@DESKTOP-IMSH0QM>
2018-11-15 14:44:47 -0400

    ua

--- Proyecto Final Programacion I/src/Logico/PUCMM.java
+++ Proyecto Final Programacion I/src/Logico/PUCMM.java
@@ -5,13 +5,21 @@ import java.util.ArrayList;
 public class PUCMM {
 	private ArrayList<Recurso> misRecursos;
 	private ArrayList<Evento> misEventos;
+	private static PUCMM pucmm;
 
-	public PUCMM() {
+	private PUCMM() {
 		misRecursos = new ArrayList<>();
 		misEventos = new ArrayList<>();
 	
 		
 		
+	}
+	public static PUCMM getInstance() {
+		if(pucmm == null) {
+			pucmm = new PUCMM();
+		}
+		
+		return pucmm;
 	}
 
 }
