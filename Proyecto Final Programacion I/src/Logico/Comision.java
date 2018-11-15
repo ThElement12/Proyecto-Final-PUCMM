1c6090bd47b47e689eae2a4a1029e2917f1e5a44
Joseph <Joseph@DESKTOP-IMSH0QM>
2018-11-15 15:11:53 -0400

    ICKKCK

---
+++ Proyecto Final Programacion I/src/Logico/Comision.java
@@ -0,0 +1,31 @@
+package Logico;
+
+import java.util.ArrayList;
+
+public class Comision {
+	private ArrayList<Persona> misMiembros;
+	private String area;
+	
+	public Comision(String area) {
+		this.area = area;
+		misMiembros = new ArrayList<>();
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public void insertarJuez(Juez miJuez) {
+		misMiembros.add(miJuez);
+	}
+	public void insertarPrincipante(Participante miParticipante) {
+		misMiembros.add(miParticipante);
+	}
+	
+	
+	
+
+}
