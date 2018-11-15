d2f12a5df08e8af9abfb6a8a9e6fb0ff075fa1ce
ruben <ruben@DESKTOP-CV5HDJ7>
2018-11-15 15:15:03 -0400

    La clase Trabajo fue creada, tambien se arreglaron varios bugs. Por Ejemplo:
    -La interfaz grafica ya no se puede abrir por razones tecnicas que no se
    pueden discutir con usted(es).
    
    						Att. El Jefe

--- Proyecto Final Programacion I/src/Logico/Trabajo.java
+++ Proyecto Final Programacion I/src/Logico/Trabajo.java
@@ -1,5 +1,40 @@
 package Logico;
 
 public class Trabajo {
+	private String nombre;
+	private Participante participante;
+	private String area;
+	
+	public Trabajo(String nombre, Participante participante, String area) {
+		super();
+		this.nombre = nombre;
+		this.participante = participante;
+		this.area = area;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Participante getParticipante() {
+		return participante;
+	}
+
+	public void setParticipante(Participante participante) {
+		this.participante = participante;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+	
 }
