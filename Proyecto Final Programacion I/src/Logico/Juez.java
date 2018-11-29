2509b8bdf98249f8327cba4292bae32ed61a871b
Joseph <Joseph@DESKTOP-IMSH0QM>
2018-11-29 15:33:33 -0400

    sa

--- Proyecto Final Programacion I/src/Logico/Juez.java
+++ Proyecto Final Programacion I/src/Logico/Juez.java
@@ -4,11 +4,13 @@ import java.io.Serializable;
 
 public class Juez extends Persona {
 	private boolean representante;
+	private String area;
 	private String tema;
-	public Juez(String cedula, String nombre, String numero, Evento evento, Comision comision, boolean representante, String tema) {
+	public Juez(String cedula, String nombre, String numero, Evento evento, Comision comision, boolean representante, String area, String tema) {
 		super(cedula, nombre, numero, evento,comision);
 		this.representante = representante;
 		this.tema = tema;
+		this.setArea(area);
 	}
 	public boolean isRepresentante() {
 		return representante;
@@ -21,6 +23,12 @@ public class Juez extends Persona {
 	}
 	public void setTema(String tema) {
 		this.tema = tema;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
 	}
 	
 
---
+++ Proyecto Final Programacion I/src/Visual/RegPersona.java
@@ -0,0 +1,45 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.BevelBorder;
+
+public class RegPersona extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	public RegPersona() {
+		setBounds(100, 100, 650, 430);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JPanel panel_principal = new JPanel();
+		panel_principal.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel_principal.setBounds(6, 6, 622, 195);
+		contentPanel.add(panel_principal);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
