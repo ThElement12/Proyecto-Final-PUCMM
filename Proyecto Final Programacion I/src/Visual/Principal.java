3899f2425b21a202b2e08412f137111eeadd8bf4
Joseph <Joseph@DESKTOP-IMSH0QM>
2018-11-30 22:43:09 -0400

    Añadido la ventana de recurso

--- Proyecto Final Programacion I/src/Visual/Principal.java
+++ Proyecto Final Programacion I/src/Visual/Principal.java
@@ -121,6 +121,18 @@ public class Principal extends JFrame {
 			}
 		});
 		mnPersonas.add(mntmRegistrarPersonas);
+	
+		JMenu mnRecurso = new JMenu("Recurso");
+		menuBar.add(mnRecurso);
+		JMenuItem mntmAgregarRecurso = new JMenuItem("Agregar Recurso");
+		mntmAgregarRecurso.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegRecursos registrarRecurso = new RegRecursos();
+				registrarRecurso.setModal(true);
+				registrarRecurso.setVisible(true);
+			}
+		});
+		mnRecurso.add(mntmAgregarRecurso);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
