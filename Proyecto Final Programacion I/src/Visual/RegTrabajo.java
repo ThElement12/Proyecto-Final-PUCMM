32884b0d851285c1a7109c6b7183d335f8d29571
ruben <ruben@DESKTOP-CV5HDJ7>
2018-12-04 18:12:30 -0400

    Ventana RegTrabajo lista

--- Proyecto Final Programacion I/src/Visual/RegTrabajo.java
+++ Proyecto Final Programacion I/src/Visual/RegTrabajo.java
@@ -11,6 +11,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTable;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.border.TitledBorder;
@@ -18,7 +19,11 @@ import javax.swing.table.DefaultTableModel;
 
 import Logico.Comision;
 import Logico.Evento;
+import Logico.PUCMM;
+import Logico.Participante;
 import Logico.Persona;
+import Logico.Trabajo;
+
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
@@ -37,14 +42,17 @@ public class RegTrabajo extends JDialog {
 	private int index;
 	private String select = "";
 	private JButton btnAsignar;
-	private JButton okButton;
 	private JButton cancelButton;
+	private Comision miComision;
+	private Evento miEvento;
 	
 
 	/**
 	 * Create the dialog.
 	 */
 	public RegTrabajo(Comision miComision, Evento miEvento) {
+		this.miComision = miComision;
+		this.miEvento = miEvento;
 		setBounds(100, 100, 589, 369);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -57,9 +65,9 @@ public class RegTrabajo extends JDialog {
 		contentPanel.add(pnl_info_setter);
 		pnl_info_setter.setLayout(null);
 		
-		JComboBox cbxPosicion = new JComboBox();
+		JComboBox<Object> cbxPosicion = new JComboBox<Object>();
 		cbxPosicion.setFont(new Font("Malgun Gothic", Font.ITALIC, 12));
-		cbxPosicion.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "L\u00EDder", "Co-L\u00EDder", "Orador 1", "Orador 2", "Organizador 1", "Organizador 2"}));
+		cbxPosicion.setModel(new DefaultComboBoxModel<Object>(new String[] {"<Seleccione>", "L\u00EDder", "Co-L\u00EDder", "Orador 1", "Orador 2", "Organizador 1", "Organizador 2"}));
 		cbxPosicion.setBounds(415, 47, 128, 20);
 		pnl_info_setter.add(cbxPosicion);
 		
@@ -111,26 +119,27 @@ public class RegTrabajo extends JDialog {
 		String columNames[] = {"Id","Cédula","Nombre"};
 		model.setColumnIdentifiers(columNames);
 		table.setModel(model);
-		
+		loadTable();
 		pnl_Tabla.add(table, BorderLayout.CENTER);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				okButton = new JButton("Registrar");
-				okButton.setEnabled(false);
-				okButton.setFont(new Font("Malgun Gothic", Font.ITALIC, 12));
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
 			
 			btnAsignar = new JButton("Asignar Posici\u00F3n");
 			btnAsignar.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
+					Participante miParticipante =(Participante) PUCMM.pucmm().searchById(select);
+					Trabajo miTrabajo = searchTrabajoByPosition(cbxPosicion.getSelectedItem().toString());
+					if(miTrabajo.isDisponible()) {
+						miTrabajo.setParticipante(miParticipante);
+						miParticipante.agregarTrabajo(miTrabajo);
+						JOptionPane.showMessageDialog(null, "OperacionCompletada con éxito", "Información", JOptionPane.INFORMATION_MESSAGE);
+					}
 					
-					okButton.setEnabled(true);
+					else {
+						JOptionPane.showMessageDialog(null, "La posición ya está ocupada", "Información", JOptionPane.INFORMATION_MESSAGE);
+					}
 				}
 			});
 			btnAsignar.setEnabled(false);
@@ -138,7 +147,12 @@ public class RegTrabajo extends JDialog {
 			
 			buttonPane.add(btnAsignar);
 			{
-				cancelButton = new JButton("Cancelar");
+				cancelButton = new JButton("Salir");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						dispose();
+					}
+				});
 				cancelButton.setFont(new Font("Malgun Gothic", Font.ITALIC, 12));
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
@@ -151,11 +165,31 @@ public class RegTrabajo extends JDialog {
 		fila = new Object[model.getColumnCount()];
 		
 		for(int i = 0; i < RegComision.getMiPersona().size(); i ++) {
-			fila[0] = RegComision.getMiPersona().get(i).getId();
-			fila[1] = RegComision.getMiPersona().get(i).getCedula();
-			fila[2] = RegComision.getMiPersona().get(i).getNombre();
+			if(RegComision.getMiPersona().get(i) instanceof Participante) {
+				fila[0] = RegComision.getMiPersona().get(i).getId();
+				fila[1] = RegComision.getMiPersona().get(i).getCedula();
+				fila[2] = RegComision.getMiPersona().get(i).getNombre();
 			
-			model.addRow(fila);
+				model.addRow(fila);
+			}
 		}
+	}
+	
+	private Trabajo searchTrabajoByPosition(String nombre) {
+		Trabajo miTrabajo = null;
+		boolean finded = false;
+		int i = 0;
+		while(i < 6 && !finded) {
+			if(miComision.getMisTrabajos()[i].getPosicion().equalsIgnoreCase(nombre)) {
+				miTrabajo = miComision.getMisTrabajos()[i];
+				finded = true;
+			}
+			
+			else {
+				i ++;
+			}
+		}
+		
+		return miTrabajo;
 	}
 }
